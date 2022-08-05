c7b6d8d2801cf18640708b2bc10249906be3707f
“ธนวรรธน์ <lixkychild@gmail.com>
2022-08-05 08:39:12 +0700

    first commit

---
+++ resources/views/includes/promote/head.blade.php
@@ -0,0 +1,29 @@
+  <!-- Favicons -->
+  <link href="{{ asset('template/promote/assets/img/favicon.png') }}  " rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,300i,400,400i,600,600i,700,700i|Satisfy|Comic+Neue:300,300i,400,400i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="{{asset('template/promote/assets/vendor/animate.css/animate.min.css')}}" rel="stylesheet">
+  <link href="{{asset('template/promote/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
+  <link href="{{asset('template/promote/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
+  <link href="{{asset('template/promote/assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
+  <link href="{{asset('template/promote/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
+  <link href="{{asset('template/promote/assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="{{asset('template/promote/assets/css/style.css')}}" rel="stylesheet">
+
+
+  <!-- Vendor JS Files -->
+  <script src="{{asset('template/promote/assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+  <script src="{{asset('template/promote/assets/vendor/glightbox/js/glightbox.min.js')}}"></script>
+  <script src="{{asset('template/promote/assets/vendor/isotope-layout/isotope.pkgd.min.js')}}"></script>
+  <script src="{{asset('template/promote/assets/vendor/swiper/swiper-bundle.min.js')}}"></script>
+  <script src="{{asset('template/promote/assets/vendor/php-email-form/validate.js')}}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
---
+++ resources/views/includes/promote/header.blade.php
@@ -0,0 +1,54 @@
+<!-- ======= Top Bar ======= -->
+<section id="topbar" class="d-flex align-items-center fixed-top topbar-transparent">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-center justify-content-lg-start">
+      <i class="bi bi-phone d-flex align-items-center"><span>+66 11222345</span></i>
+      <i class="bi bi-clock ms-4 d-none d-lg-flex align-items-center"><span>Mon-Sat: 11:00 AM - 23:00 PM</span></i>
+    </div>
+  </section>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center header-transparent">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-between">
+
+      <div class="logo me-auto">
+        <h1><a href="index.php">บ้านสวนเห็ด</a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="#hero">หน้าแรก</a></li>
+          <li><a class="nav-link scrollto" href="#about">เกี่ยวกับ</a></li>
+          <li class="dropdown"><a href="#"><span>วิธีปลูกเห็ด</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">เห็ดโคนน้อย</a></li>
+                <ul>
+                  <li><a href="#">เห็ดโคนน้อย</a></li>
+                  <li><a href="#">เห็ดขอนขาว</a></li>
+                  <li><a href="#">เห็ดขอนดำ</a></li>
+                  <li><a href="#">เห็ดนางฟ้า</a></li>
+                  <li><a href="#">เห็ดภูฐาน</a></li>
+                  <li><a href="#">เห็ดหลินจือ</a></li>
+                </ul>
+              </li>
+              <li><a href="#">เห็ดขอนขาว</a></li>
+              <li><a href="#">เห็ดขอนดำ</a></li>
+              <li><a href="#">เห็ดนางฟ้า</a></li>
+              <li><a href="#">เห็ดภูฐาน</a></li>
+              <li><a href="#">เห็ดหลินจือ</a></li>
+            </ul>
+          </li>
+          <li><a class="nav-link scrollto" href="#specials">รายละเอียดสินค้า</a></li>
+          <li class="dropdown"><a href="/resources/views/auth/login.blade.php"><span>เข้าสู่ระบบ</span> <i class="bi bi-chevron-down"></i></a>
+            <ul>
+              <li><a href="#">สมัครสมาชิก</a></li>
+          </li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="#book-a-table" class="book-a-table-btn scrollto">ติดต่อสอบถาม</a>
+
+    </div>
+  </header><!-- End Header -->
