15d0c34a85d10804cfb286d30fabd409ffd9ac5b
Thanawat030 <109572234+Thanawat030@users.noreply.github.com>
2022-08-05 10:08:45 +0700

    ใส่ login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,35 +1,24 @@
 @extends('layouts.admin.master')
 
 @section('text')
-<div class="limiter">
-		<div class="container-login100" style="background-image: url('images/bg-01.jpg');">
-			<div class="wrap-login100">
-				<form class="login100-form validate-form">
-					<span class="login100-form-logo">
-						<i class="zmdi zmdi-landscape"></i>
-					</span>
-
-					<span class="login100-form-title p-b-34 p-t-27">
-						Log in
+<form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
+@csrf
+					<center>
+						<img src="{{ asset('admin/images/logo.png')}}">
+					</center>
+					<span class="login100-form-title p-b-34 p-t-15">
+						<h1>เข้าสู่ระบบ</h1>
 					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Enter username">
-						<input class="input100" type="text" name="username" placeholder="Username">
+						<input class="input100" type="text" name="username" placeholder="Username" id="username">
 						<span class="focus-input100" data-placeholder="&#xf207;"></span>
 					</div>
 
 					<div class="wrap-input100 validate-input" data-validate="Enter password">
-						<input class="input100" type="password" name="pass" placeholder="Password">
+						<input class="input100" type="password" name="password" placeholder="Password" id="password">
 						<span class="focus-input100" data-placeholder="&#xf191;"></span>
 					</div>
-
-					<div class="contact100-form-checkbox">
-						<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
-						<label class="label-checkbox100" for="ckb1">
-							Remember me
-						</label>
-					</div>
-
 					<div class="container-login100-form-btn">
 						<button class="login100-form-btn">
 							Login
@@ -37,13 +26,9 @@
 					</div>
 
 					<div class="text-center p-t-90">
-						<a class="txt1" href="#">
+						<a class="txt1" href="{{ route('password.request') }}">
 							Forgot Password?
 						</a>
 					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-    <div id="dropDownSelect1"></div>
+</form>
 @endsection
