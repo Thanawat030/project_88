11a8ef165a2f38cfbeecff8399a8cfe059403e5b
Thanawat030 <109572234+Thanawat030@users.noreply.github.com>
2022-08-09 09:43:09 +0700

    อัพหน้าเห็ด

--- resources/views/includes/promote/header.blade.php
+++ resources/views/includes/promote/header.blade.php
@@ -40,7 +40,7 @@
             </ul>
           </li>
           <li><a class="nav-link scrollto" href="#specials">รายละเอียดสินค้า</a></li>
-          <li class="dropdown"><a href="#"><span>เข้าสู่ระบบ</span> <i class="bi bi-chevron-down"></i></a>
+          <li class="dropdown"><a href=""><span>เข้าสู่ระบบ</span> <i class="bi bi-chevron-down"></i></a>
             <ul>
               <li><a href="#">สมัครสมาชิก</a></li>
           </li>
