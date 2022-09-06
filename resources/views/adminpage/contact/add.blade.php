16384211409c7c1e785fbc1938b95cf0beee2b62
Chonlada111 <109642086+Chonlada111@users.noreply.github.com>
2022-09-06 16:22:32 +0700

    หน้า admin

--- resources/views/adminpage/contact/add.blade.php
+++ resources/views/adminpage/contact/add.blade.php
@@ -7,7 +7,7 @@
             <div class="col-md-12 grid-margin stretch-card">
               <div class="card">
                 <div class="card-body">
-                  <h4 class="card-title">เพิ่มไขติดต่อ</h4>
+                  <h4 class="card-title">เพิ่มติดต่อ</h4>
                   <p class="card-description">
                   </p>
                   <form action="{{ route('adminpage.contact.add')}}" method="POST" class="forms-sample">
